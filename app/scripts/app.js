5fba9e0ae42a00bd125aed63724c13c8fe22491c
Jefferson Neves <jefferson.rpn@gmail.com>
2016-05-12 11:31:07 -0300

    DevOps for the win!

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name labAnalyticsLogoApp
+ * @description
+ * # labAnalyticsLogoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('labAnalyticsLogoApp', []);
